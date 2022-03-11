d5362556b5e82d09b00793bbff977fafff8246c6
BongumusaSizwe <1682836@students.wits.ac.za>
2022-03-11 11:40:59 +0200

    update

--- bots/python/unisa.py
+++ bots/python/unisa.py
@@ -11,14 +11,51 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
 
+
+details = {
+    'name': 'Tholwana',
+    'surname': 'Maloma',
+    'cell':'+27617095670',
+    'id':'0403110957076',
+    'addr':'13719 Kuthangi Street, Palmrideg Ext 8, 1458',
+    'parent_name': 'Nomsa Gladys Maloma',
+    'parent_cell': '+27849470796',
+    'year': '04',
+    'month': '03',
+    'day':'11',
+}
+
 browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+# browser = webdriver.Firefox()
 #Open unisa online application portal:Caution: Elements change often
 browser.get(
     'https://www.unisa.ac.za/sites/corporate/default/Apply-for-admission/Apply-for-admission-to-study:-application-tool'
     )
 
-#Wait till  elements show up
+# Wait till  elements show up
 
-wait = WebDriverWait(browser, 60)
-#Click on undergrad
-wait.until(EC.element_to_be_clickable((By.ID, 'UD'))).click()
+wait = WebDriverWait(browser,45)
+delay = 40
+try:
+    ud = WebDriverWait(browser, delay),until(EC.presence_of_elements_located((By.ID, 'UD')))
+    print('Page is readt')
+    browser.find_element_by_id('UD').click()
+except:
+    print("Page took too long to load")
+
+WebDriverWait(browser, delay).until(EC.presence_of_elements_located((By.ID, 'showNO')))
+browser.find_element_by_id('showNO').click()
+
+
+WebDriverWait(browser, delay).until(EC.presence_of_elements_located((By.NAME, 'student.surname')))
+browser.find_element_by_name('student.surname').send_keys(details['surname'])
+browser.find_element_by_name('student.firstnames').send_keys(details['name'])
+
+browser.find_element_by_name('student.birthYear').select_by_value(details['year'])
+browser.find_element_by_name('student.birthMonth').select_by_value(details['month'])
+browser.find_element_by_name('student.birthMonth').select_by_value(details['month'])
+browser.find_element_by_name('student.birthDay').select_by_value(details['day'])
+
+browser.implicitly_wait(10)
+
+browser.execute_script('validate()')
